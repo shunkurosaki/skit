fa1639f63c735dd42f56aa35f31dd970f1d357bf
shunkurosaki <mouippatukamasouze@gmail.com>
2014-11-25 15:16:47 -0800

    close send purchase

--- send.php
+++ send.php
@@ -36,6 +36,9 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script type="text/javascript">
+    mixpanel.track("Purchased");
+    </script>
 </head>
 
 <body>
